f3b6f09ec538d6e6330426d174837c208fc7f868
Roman Khegay <romankhegay@hotmail.com>
2022-08-25 14:12:15 +0600

    chore: structure update
    [skip ci]

--- lib/_core/types/boolean-input.ts
+++ lib/_core/types/boolean-input.ts
@@ -1,2 +1,1 @@
 export type BooleanInput = boolean | string | undefined | null;
-export type NumberInput = number | string | undefined | null;
--- projects/ngwr/core/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './convert-input';
-export * from './indexable';
-export * from './ng-class';
