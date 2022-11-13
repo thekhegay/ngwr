8b1f3e0a0cfef63fbb9d8d1e8ad4cf4ce152d189
Roman Khegay <romankhegay@hotmail.com>
2022-11-13 20:25:00 +0600

    Release/0.2.0 (#18)
    * chore(release): 0.2.0
    
    * chore(): fix release scripts

--- scripts/shared/copy-assets.ts
+++ scripts/shared/copy-assets.ts
@@ -1,10 +1,8 @@
-import { copyFile, copyFileSync, readFileSync, writeFileSync } from 'fs';
+import { copyFileSync, writeFileSync } from 'fs';
 
 export function copyAssets(): void {
   const distPath = 'dist/lib';
   copyFileSync('projects/showcase/src/assets/images/logo.svg', `${distPath}/logo.svg`);
-  let readmeFile = readFileSync('README.md').toString();
-  readmeFile = readmeFile.replace('projects/showcase/src/assets/images/logo.svg', 'logo.svg');
-  writeFileSync(`${distPath}/README.md`, readmeFile);
+  writeFileSync('README.md', `${distPath}/README.md`);
   copyFileSync('LICENSE', `${distPath}/LICENSE`);
 }
