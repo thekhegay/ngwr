9923f93262ffa42712cfd8fb9189000d44e12c41
Roman Khegay <romankhegay@hotmail.com>
2022-11-22 21:14:06 +0600

    fix(scripts): add README.md copy to dist

--- scripts/shared/copy-assets.ts
+++ scripts/shared/copy-assets.ts
@@ -1,8 +1,8 @@
-import { copyFileSync, writeFileSync } from 'fs';
+import { copyFileSync } from 'fs';
 
 export function copyAssets(): void {
   const distPath = 'dist/lib';
   copyFileSync('projects/showcase/src/assets/images/logo.svg', `${distPath}/logo.svg`);
-  writeFileSync('README.md', `${distPath}/README.md`);
+  copyFileSync('README.md', `${distPath}/README.md`);
   copyFileSync('LICENSE', `${distPath}/LICENSE`);
 }
