137d531553a7395c764f04874de76ac7fbb4bc14
Roman Khegay <romankhegay@hotmail.com>
2022-11-22 21:56:17 +0600

    perf(scripts): remove logo.svg from build-dist

--- scripts/shared/copy-assets.ts
+++ scripts/shared/copy-assets.ts
@@ -2,7 +2,6 @@ import { copyFileSync } from 'fs';
 
 export function copyAssets(): void {
   const distPath = 'dist/lib';
-  copyFileSync('projects/showcase/src/assets/images/logo.svg', `${distPath}/logo.svg`);
   copyFileSync('README.md', `${distPath}/README.md`);
   copyFileSync('LICENSE', `${distPath}/LICENSE`);
 }
