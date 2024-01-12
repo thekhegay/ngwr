c1f485316a7715c15f2f0d4ed70bd2914276dc9e
Roman Khegay <rk@garuna.dev>
2024-01-12 06:54:10 +0600

    feat(checkbox): update page title

--- projects/showcase/src/app/components/checkbox/checkbox.component.ts
+++ projects/showcase/src/app/components/checkbox/checkbox.component.ts
@@ -13,16 +13,17 @@ export class CheckboxComponent implements OnInit {
 
   readonly importCode: string =
     "import { WrCheckboxModule } from 'ngwr/checkbox';\n\n@NgModule({\n  imports: [\n    // ...\n    WrCheckboxModule,\n  ],\n  // ...\n})\nexport class MyModule {}";
-  readonly usageCode = `<wr-checkbox [ngModel]="true"></wr-checkbox>`;
-  readonly disabledCode = '<wr-checkbox [disabled]="true"></wr-checkbox>';
-  readonly valueChangeCode = '<wr-checkbox [ngModel]="false" (ngModelChange)="onValueChange($event)"></wr-checkbox>';
-  readonly ngModelChangeReturn = 'EventEmitter<any>';
+  readonly usageCode: string = `<wr-checkbox [ngModel]="true"></wr-checkbox>`;
+  readonly disabledCode: string = '<wr-checkbox [disabled]="true"></wr-checkbox>';
+  readonly valueChangeCode: string =
+    '<wr-checkbox [ngModel]="false" (ngModelChange)="onValueChange($event)"></wr-checkbox>';
+  readonly ngModelChangeReturn: string = 'EventEmitter<any>';
 
   constructor(private readonly seoService: SeoService) {}
 
   ngOnInit(): void {
     this.seoService.setCanonicalURL();
-    this.seoService.setTitle('Checkbox');
+    this.seoService.setTitle(['Checkbox', 'Components']);
     this.seoService.setDescription(this.description);
     this.seoService.setKeywords(['checkbox', 'wr-checkbox']);
   }
