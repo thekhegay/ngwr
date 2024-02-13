d144ff25ee11c601fa44c557d7bf80caf8a266ef
Roman Khegay <rk@garuna.dev>
2024-02-14 02:18:22 +0600

    fix(actions): fix showcase version link

--- scripts/postbump.ts
+++ scripts/postbump.ts
@@ -5,7 +5,10 @@ import { execute } from './shared/execute';
 import { version } from '../projects/lib/package.json';
 
 (function main(): void {
-  writeFileSync(resolve('./projects/showcase/app/@shared/version.ts'), `export const NGWR_VERSION = '${version}';\n`, {
+  writeFileSync(
+    resolve('./projects/showcase/src/app/@shared/version.ts'),
+    `export const NGWR_VERSION = '${version}';\n`,
+    {
     encoding: 'utf-8',
   });
   execute('git add .');
