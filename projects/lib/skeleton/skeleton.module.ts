1e58cc93b4577dd2ff99960edb9765c364b2bac8
Roman Khegay <rk@garuna.dev>
2024-03-20 15:42:53 +0500

    refactor: make skeleton standalone

---
+++ projects/lib/skeleton/skeleton.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { WrSkeletonComponent } from './skeleton.component';
+
+@NgModule({
+  imports: [WrSkeletonComponent],
+  exports: [WrSkeletonComponent],
+})
+export class WrSkeletonModule {}
