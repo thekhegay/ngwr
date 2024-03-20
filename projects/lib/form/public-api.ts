902a355a4682e45938c140bc1292465876e0e29e
Roman Khegay <rk@garuna.dev>
2024-03-20 16:21:26 +0500

    refactor: make form standalone

--- projects/lib/form/public-api.ts
+++ projects/lib/form/public-api.ts
@@ -5,4 +5,6 @@
  * found in the LICENSE file at https://github.com/thekhegay/ngwr/blob/main/LICENSE
  */
 
-export * from './public-api';
+export * from './form-error.component';
+export * from './form-item.component';
+export * from './form.module';
