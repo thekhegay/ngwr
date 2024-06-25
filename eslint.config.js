abb7141f40b1bdcb3ac088a56a51d8363858cf58
Roman Khegay <rk@garuna.dev>
2024-06-26 03:28:25 +0500

    chore: setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 const eslint = require('@eslint/js');
 const tsEslint = require('typescript-eslint');
 const ngEslint = require('angular-eslint');
+const prettierEslintRecommended = require('eslint-plugin-prettier/recommended');
+const importX = require('eslint-plugin-import-x');
 
 module.exports = tsEslint.config(
   {
@@ -11,25 +13,140 @@ module.exports = tsEslint.config(
       ...tsEslint.configs.recommended,
       ...tsEslint.configs.stylistic,
       ...ngEslint.configs.tsRecommended,
+      prettierEslintRecommended
     ],
     processor: ngEslint.processInlineTemplates,
+    plugins: {
+      'import-x': importX,
+    },
     rules: {
-      '@angular-eslint/directive-selector': [
+      '@angular-eslint/no-async-lifecycle-method': 'error',
+      '@angular-eslint/no-attribute-decorator': 'error',
+      '@angular-eslint/sort-lifecycle-methods': 'warn',
+      '@angular-eslint/component-max-inline-declarations': [
         'error',
         {
-          type: 'attribute',
-          prefix: 'wr',
-          style: 'camelCase',
-        },
+          'template': 1,
+          'styles': 0,
+          'animations': 0
+        }
       ],
       '@angular-eslint/component-selector': [
         'error',
         {
-          type: 'element',
-          prefix: 'wr',
+          type: ['element', 'attribute'],
+          prefix: ['wr', 'ngwr'],
           style: 'kebab-case',
         },
       ],
+      '@angular-eslint/consistent-component-styles': 'error',
+      '@angular-eslint/contextual-decorator': 'error',
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: ['wr', 'ngwr'],
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/no-conflicting-lifecycle': 'error',
+      '@angular-eslint/no-forward-ref': 'error',
+      '@angular-eslint/no-lifecycle-call': 'error',
+      '@angular-eslint/no-pipe-impure': 'error',
+      '@angular-eslint/no-queries-metadata-property': 'error',
+      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      '@angular-eslint/prefer-output-readonly': 'error',
+      '@angular-eslint/prefer-standalone': 'error',
+      '@angular-eslint/relative-url-prefix': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'error',
+      '@angular-eslint/use-pipe-transform-interface': 'error',
+      '@angular-eslint/directive-class-suffix': [
+        'error',
+        {
+          'suffixes': ['Directive', 'Component']
+        }
+      ],
+      '@angular-eslint/component-class-suffix': [
+        'error',
+        {
+          'suffixes': ['Component']
+        }
+      ],
+      "@typescript-eslint/consistent-type-definitions": "error",
+      "@typescript-eslint/no-unused-expressions": "error",
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          "allowExpressions": true,
+          "allowConciseArrowFunctionExpressionsStartingWithVoid": true
+        }
+      ],
+      "@typescript-eslint/no-inferrable-types": "error",
+      "import-x/no-empty-named-blocks": "error",
+      "import-x/no-duplicates": "error",
+      "import-x/no-unused-modules": "error",
+      "import-x/no-unassigned-import": "error",
+      "import-x/exports-last": "error",
+      "import-x/first": "error",
+      "import-x/newline-after-import": "error",
+      "import-x/order": [
+        "error",
+        {
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": false
+          },
+          "newlines-between": "always",
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            [
+              "sibling",
+              "index"
+            ]
+          ],
+          "pathGroups": [
+            {
+              "pattern": "@angular/**",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "{rxjs,rxjs/**}",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "@ngrx/**",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "{ng-zorro-antd/**,ngwr/**,@jsverse/**}",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "{#api,#core/**,#assets/**,#env/**,#app/**,#routing,#store/**}",
+              "group": "internal",
+              "position": "before"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": []
+        }
+      ],
+      "no-bitwise": "error",
+      "no-irregular-whitespace": "error",
+      "no-multiple-empty-lines": "error",
+      "no-redeclare": "error",
+      "no-sparse-arrays": "error",
+      "no-template-curly-in-string": "error",
+      "prefer-object-spread": "error",
+      "prefer-template": "error"
+
+
     },
   },
   {
@@ -37,7 +154,15 @@ module.exports = tsEslint.config(
     extends: [
       ...ngEslint.configs.templateRecommended,
       ...ngEslint.configs.templateAccessibility,
+      prettierEslintRecommended,
     ],
-    rules: {}
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          'parser': 'angular'
+        }
+      ]
+    }
   }
 )
