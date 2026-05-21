c00704dd4a231b2d66f67398bc3e27dc20539650
rk <rk@garuna.dev>
2026-05-21 11:57:43 +0500

    feat(showcase): sort sidebar alphabetically and add active line indicator

--- projects/showcase/app/_layout/sidebar/sidebar.component.ts
+++ projects/showcase/app/_layout/sidebar/sidebar.component.ts
@@ -51,6 +51,35 @@ export class SidebarComponent {
       title: 'Components',
       categories: [
         {
+          title: 'Button',
+          children: [
+            { title: 'Button', url: ['/docs', 'components', 'button'] },
+            { title: 'Button Group', url: ['/docs', 'components', 'button-group'] },
+          ],
+        },
+        {
+          title: 'Data',
+          children: [
+            { title: 'Pagination', url: ['/docs', 'components', 'pagination'] },
+            { title: 'Table', url: ['/docs', 'components', 'table'] },
+          ],
+        },
+        {
+          title: 'Display',
+          children: [
+            { title: 'Alert', url: ['/docs', 'components', 'alert'] },
+            { title: 'Avatar', url: ['/docs', 'components', 'avatar'] },
+            { title: 'Badge', url: ['/docs', 'components', 'badge'] },
+            { title: 'Divider', url: ['/docs', 'components', 'divider'] },
+            { title: 'Icon', url: ['/docs', 'components', 'icon'] },
+            { title: 'Progress', url: ['/docs', 'components', 'progress'] },
+            { title: 'QR', url: ['/docs', 'components', 'qrcode'] },
+            { title: 'Skeleton', url: ['/docs', 'components', 'skeleton'] },
+            { title: 'Spinner', url: ['/docs', 'components', 'spinner'] },
+            { title: 'Tag', url: ['/docs', 'components', 'tag'] },
+          ],
+        },
+        {
           title: 'Form',
           children: [
             { title: 'Checkbox', url: ['/docs', 'components', 'checkbox'] },
@@ -64,18 +93,8 @@ export class SidebarComponent {
           ],
         },
         {
-          title: 'Button',
-          children: [
-            { title: 'Button', url: ['/docs', 'components', 'button'] },
-            { title: 'Button Group', url: ['/docs', 'components', 'button-group'] },
-          ],
-        },
-        {
-          title: 'Data',
-          children: [
-            { title: 'Pagination', url: ['/docs', 'components', 'pagination'] },
-            { title: 'Table', url: ['/docs', 'components', 'table'] },
-          ],
+          title: 'Layout',
+          children: [{ title: 'Collapse', url: ['/docs', 'components', 'collapse'] }],
         },
         {
           title: 'Navigation',
@@ -95,25 +114,6 @@ export class SidebarComponent {
             { title: 'Toast', url: ['/docs', 'components', 'toast'] },
             { title: 'Tooltip', url: ['/docs', 'components', 'tooltip'] },
           ],
-        },
-        {
-          title: 'Display',
-          children: [
-            { title: 'Alert', url: ['/docs', 'components', 'alert'] },
-            { title: 'Avatar', url: ['/docs', 'components', 'avatar'] },
-            { title: 'Badge', url: ['/docs', 'components', 'badge'] },
-            { title: 'Divider', url: ['/docs', 'components', 'divider'] },
-            { title: 'Icon', url: ['/docs', 'components', 'icon'] },
-            { title: 'Progress', url: ['/docs', 'components', 'progress'] },
-            { title: 'QR', url: ['/docs', 'components', 'qrcode'] },
-            { title: 'Skeleton', url: ['/docs', 'components', 'skeleton'] },
-            { title: 'Spinner', url: ['/docs', 'components', 'spinner'] },
-            { title: 'Tag', url: ['/docs', 'components', 'tag'] },
-          ],
-        },
-        {
-          title: 'Layout',
-          children: [{ title: 'Collapse', url: ['/docs', 'components', 'collapse'] }],
         },
       ],
     },
