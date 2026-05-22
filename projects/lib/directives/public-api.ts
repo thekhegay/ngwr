e922e83fd7b25ad28061f08d3623d4a7fcaea5d4
rk <rk@garuna.dev>
2026-05-22 15:44:31 +0500

    feat(directives): add ngwr/directives package

---
+++ projects/lib/directives/public-api.ts
@@ -0,0 +1,4 @@
+export { WrAutofocusDirective } from './autofocus.directive';
+export { WrAutosizeDirective } from './autosize.directive';
+export { WrClickOutsideDirective } from './click-outside.directive';
+export { WrCopyToClipboardDirective } from './copy-to-clipboard.directive';
