f58fe6d857f894bc3703a6d4b27528fe91a108c3
rk <rk@garuna.dev>
2026-05-25 17:36:25 +0500

    feat(directives): add [wrReveal] for scroll-triggered enter animations

--- projects/lib/directives/public-api.ts
+++ projects/lib/directives/public-api.ts
@@ -2,3 +2,4 @@ export { WrAutofocusDirective } from './autofocus.directive';
 export { WrAutosizeDirective } from './autosize.directive';
 export { WrClickOutsideDirective } from './click-outside.directive';
 export { WrCopyToClipboardDirective } from './copy-to-clipboard.directive';
+export { WrRevealDirective } from './reveal.directive';
