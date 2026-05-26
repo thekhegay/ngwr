61da2ec6cbc98e4adac9238f02179038da8ae589
rk <rk@garuna.dev>
2026-05-26 10:28:07 +0500

    feat(collapse): add dedicated <wr-accordion> component

--- projects/lib/collapse/public-api.ts
+++ projects/lib/collapse/public-api.ts
@@ -1,3 +1,4 @@
 export { WrCollapseComponent } from './collapse.component';
 export { WrCollapseGroupComponent } from './collapse-group.component';
+export { WrAccordionComponent } from './accordion.component';
 export { WR_COLLAPSE_GROUP, type WrCollapseGroupContext } from './tokens';
