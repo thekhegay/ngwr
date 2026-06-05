cd3c218bb30fd09ef6cd5afde04c3201591d170c
rk <rk@garuna.dev>
2026-06-05 16:54:21 +0500

    refactor(icon): adopt lucide visual style for lib-internal svgs (no peer dep)

--- projects/lib/table/table-sort.ts
+++ projects/lib/table/table-sort.ts
@@ -18,7 +18,7 @@ import type { WrTableSortDirection } from './types';
 @Component({
   selector: 'wr-table-sort',
   template:
-    '<svg class="wr-icon__svg wr-table-sort__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" aria-hidden="true"><path fill="currentColor" d="M71.1 98.5v-54l14.4 14.4c2.5 2.5 6.6 2.7 9.1.3 2.6-2.5 2.7-6.6.1-9.2L69.5 24.8c-2.7-2.7-7-2.7-9.7 0l-25 25c-2.5 2.5-2.7 6.6-.3 9.1 2.5 2.6 6.6 2.7 9.2.1l14.6-14.6v54.3c0 3.6 3 6.5 6.6 6.4 3.5-.1 6.2-3.1 6.2-6.6"/></svg>',
+    '<svg class="wr-icon__svg wr-table-sort__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>',
   encapsulation: ViewEncapsulation.None,
   host: { '[class]': 'classes()' },
 })
