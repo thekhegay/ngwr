42f5a0b9da6524e9798129a528207a3f9ff6c92b
rk <rk@garuna.dev>
2026-09-04 23:44:07 +0500

    feat(showcase): the docs site gets search, on the palette it just grew

--- projects/showcase/app/_core/services/index.ts
+++ projects/showcase/app/_core/services/index.ts
@@ -1,2 +1,3 @@
+export { DocsSearch, hitPath } from './search';
 export { MetaService } from './meta';
 export { PrimaryColor, PRIMARY_PRESETS, DEFAULT_PRIMARY_ID, type PrimaryPreset } from './primary-color';
